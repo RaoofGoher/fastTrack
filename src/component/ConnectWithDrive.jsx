3d2ff55ab8c2c3cb5d0cf983359210148c1e0eb5
Raoof Goher <raoofgohersd@gmail.com>
2024-11-14 03:55:45 +0500

    minor changes

--- src/component/ConnectWithDrive.jsx
+++ src/component/ConnectWithDrive.jsx
@@ -14,7 +14,7 @@ const ConnectWithDriveCard = () => {
 
       <div className="p-4 bg-gray-100 text-center">
         {/* Redirect to OAuth page */}
-        <Link to="https://api.fastrakconnect.com/google-drive-oauth/?redirect_uri=https://your-app-url.com/oauth-callback">
+        <Link to="https://api.fastrakconnect.com/google-drive-oauth/?redirect_uri=https://api.fastrakconnect.com/oauth-callback">
           <button className="flex items-center justify-center px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600">
             <FaGoogleDrive className="w-5 h-5 mr-2" /> Connect with Drive
           </button>
